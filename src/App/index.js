1c7fad09fd3764518af2ee2893230bda5c6940bd
Dmytro Artiuk <dimairak88@wp.pl>
2024-11-11 22:26:27 +0100

    add Normalize

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,14 @@
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
-
-
+import { Normalize } from "styled-normalize";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Normalize />
       <GlobalStyle />
     </ThemeProvider>
-
   );
 }
 
