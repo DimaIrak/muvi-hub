91215de2d6313c74f5a19f13ef7a4db8fb92856f
Dmytro Artiuk <dimairak88@wp.pl>
2025-03-26 11:48:53 +0100

    removed unnecessary code from index.js MuviList

--- src/features/MuviList/index.js
+++ src/features/MuviList/index.js
@@ -1,11 +1,2 @@
-const fetchMovies = async () => {
-    try {
-      const data = await getMovieListFromApi();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  
-  fetchMovies();
+
   
